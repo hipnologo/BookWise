991bf7413cff7e9c597d6281f8ff00d2fa3fb9d2
Fabio <21972160+hipnologo@users.noreply.github.com>
2024-04-04 09:14:08 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+app.get('/isbn/:isbn', async (req, res) => {
+  const { isbn } = req.params;
+  const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
+  const data = await response.json();
+  res.send(data);
+});
+
+app.get('/title/:title', async (req, res) => {
+  const { title } = req.params;
+  const response = await fetch(`http://openlibrary.org/search.json?title=${title}`);
+  const data = await response.json();
+  res.send(data.docs[0]); // Return the first result
+});
+
+app.get('/author/:author', async (req, res) => {
+  const { author } = req.params;
+  const response = await fetch(`http://openlibrary.org/search.json?author=${author}`);
+  const data = await response.json();
+  res.send(data.docs); // Return all books by the author
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
